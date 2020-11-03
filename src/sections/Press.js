1a250afdc673443c510f2c9ea94ecf9852748743
skuttenkuler <sam.kuttenk@gmail.com>
2020-11-02 22:46:38 -0800

    state rendering and hiding component

--- src/sections/Press.js
+++ src/sections/Press.js
@@ -3,18 +3,29 @@ import React from 'react';
 import { BiChevronDownCircle } from 'react-icons/bi';
 import { BiChevronUpCircle } from 'react-icons/bi';
 
-export default function PressSection(){
-    return(
-        <div>
-        <div id="press-section" className="section">
-            <div className="main-container">
-                <h1>Press</h1>
-                <BiChevronDownCircle className="openButton"/>
-            </div>
+export default class PressSection extends React.Component{
+    state = { 
+        display: false,
+    }
+    render(){
+        const {display} = this.state;
+        return(
+            <div>
+                <div id="press-section" className="section">
+                    <div className="main-container">
+                        <h1>Press</h1>
+                        <a href="#press-dropdown"><BiChevronDownCircle className="openButton" onClick={() => this.setState({ display : !display})}/></a>
+                    </div>
+                </div>
+                { display 
+                        ? 
+                        <div id="press-dropdown" className="section-dropdown">
+                            <a href="#press-section"><BiChevronUpCircle className="closeButton" onClick={() => this.setState({ display : !display})}/></a>
+                        </div>
+                        :
+                        null
+                }
         </div>
-        <div id="about-dropdown" className="section-dropdown">
-            <BiChevronUpCircle className="closeButton"/>
-        </div>
-    </div>
-    )
+        )
+    }
 };
