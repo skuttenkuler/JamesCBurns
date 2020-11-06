ddeb7251fd46e4531e66b61d78194e8fbe329b49
skuttenkuler <sam.kuttenk@gmail.com>
2020-11-06 12:38:06 -0800

    all images rendering

--- src/sections/Gallery.js
+++ src/sections/Gallery.js
@@ -6,6 +6,40 @@ const images = [
         original: 'https://jimmyburns.s3-us-west-1.amazonaws.com/1.jpg',
         thumbnail:'https://jimmyburns.s3-us-west-1.amazonaws.com/1.jpg'
     },
+    {
+        original: 'https://jimmyburns.s3-us-west-1.amazonaws.com/3.jpeg',
+        thumbnail:'https://jimmyburns.s3-us-west-1.amazonaws.com/3.jpeg'
+    },
+    {
+        original: 'https://jimmyburns.s3-us-west-1.amazonaws.com/4.jpg',
+        thumbnail:'https://jimmyburns.s3-us-west-1.amazonaws.com/4.jpg'
+    },
+    {
+        original: 'https://jimmyburns.s3-us-west-1.amazonaws.com/5.jpg',
+        thumbnail:'https://jimmyburns.s3-us-west-1.amazonaws.com/5.jpg'
+    },
+    {
+        original: 'https://jimmyburns.s3-us-west-1.amazonaws.com/6.jpeg',
+        thumbnail:'https://jimmyburns.s3-us-west-1.amazonaws.com/6.jpeg'
+    },
+    {
+        original: 'https://jimmyburns.s3-us-west-1.amazonaws.com/7.jpg',
+        thumbnail:'https://jimmyburns.s3-us-west-1.amazonaws.com/7.jpg'
+    },
+    {
+        original: 'https://jimmyburns.s3-us-west-1.amazonaws.com/8.jpg',
+        thumbnail:'https://jimmyburns.s3-us-west-1.amazonaws.com/8.jpg'
+    },
+    {
+        original: 'https://jimmyburns.s3-us-west-1.amazonaws.com/9.jpeg',
+        thumbnail:'https://jimmyburns.s3-us-west-1.amazonaws.com/9.jpeg'
+    },
+    {
+        original: 'https://jimmyburns.s3-us-west-1.amazonaws.com/10.jpg',
+        thumbnail:'https://jimmyburns.s3-us-west-1.amazonaws.com/10.jpg'
+    },
+    
+
    
 ];
 export default class GallerySection extends React.Component{
